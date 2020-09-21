46578070638c4a675df4b4491cc4f89347da6178
rhysmaddren <rhysmaddren@hotmail.co.uk>
2020-09-21 16:39:05 +0100

    Added basic api functionality, weather type

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [cityWeather, setCityWeather] = useState(null);
+  const handleLocationClick = async () => {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=San+Fransisco&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+    const data = await res.json();
+    setCityWeather(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleLocationClick}>San Fransisco</button>
+      {cityWeather ? (
+        <div>
+          <p>{cityWeather.weather[0].main}</p>
+          <img src={`http://openweathermap.org/img/wn/${cityWeather.weather[0].icon}@2x.png`} />
+        </div>
+      ) : null}
     </div>
   );
 }

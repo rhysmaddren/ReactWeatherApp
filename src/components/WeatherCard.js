acece01bfc3ee0e9c5ce4cfc1c78105a26a3da12
rhysmaddren <rhysmaddren@hotmail.co.uk>
2020-09-24 18:27:27 +0100

    Styling with styled components

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,11 +1,37 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledWeatherCard = styled.div`
+    width:40%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #3A506B;
+    border-radius: 20px;
+
+    p {
+        font-size: 20px; ;
+        padding: 20px;
+        text-align: center;
+        margin:20px;
+        background-color: #5BC0BE;
+        flex-grow: 1;
+        border-radius: 20px;
+
+    }
+`
+
+
 
 const WeatherCard = (props) => (
-    <div>
+    <StyledWeatherCard>
         <p>{props.condition}</p>
+        <div>
+            <img src={props.icon} />
+        </div>
         <p>{props.temp}</p>
-        <img src={props.icon} />
-    </div>
+    </StyledWeatherCard>
 )
 
 export default WeatherCard;
